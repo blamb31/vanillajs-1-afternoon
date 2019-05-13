7a4c00881238564494a934985e2f26114db1cf93
Blake Lamb <blake.lamb31@gmail.com>
2019-05-13 16:09:14 -0600

    Initial Commit and completed prject. Finished.

---
+++ Part2/index.js
@@ -0,0 +1,16 @@
+idInput = document.getElementById('idInput')
+colorInput = document.getElementById('colorInput')
+
+// console.log(cardId)
+// console.log(cardStyle)
+
+function setCard() {
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+
+}
+
+
+
+
+
